7cda03ce7904da4b8c88462b560119d160f3ddb6
Leo17 <95230017+kangluoyao@users.noreply.github.com>
2025-12-20 17:18:16 +0800

    Add files via upload

---
+++ post_processing/post_processing.sh
@@ -0,0 +1,4 @@
+python convert_rosdir_to_lerobot_auto.py \
+  --raw_root /Users/leo17/Desktop/surgical_robotics/dataset/Rosbag_lerobot/rosbag_data/\
+  --out_root /Users/leo17/Desktop/surgical_robotics/dataset/Rosbag_lerobot/lerobot_out_run001\
+  --calib_xml /Users/leo17/Desktop/surgical_robotics/dataset/Rosbag_lerobot/autonomous_needle_insertion/workspace/calibration/PlusDeviceSet_fCal_Wisonic_C5_1_NDIPolaris_2.0_20251212_144952_SRIL.xml
