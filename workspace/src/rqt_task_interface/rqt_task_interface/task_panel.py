06601372c74c33551535832fff4891c013649eb6
ylzhang <swuzyl@gmail.com>
2025-12-22 17:15:29 +0800

    Add probe type, setup, and needle setup options to TaskPanel

--- workspace/src/rqt_task_interface/rqt_task_interface/task_panel.py
+++ workspace/src/rqt_task_interface/rqt_task_interface/task_panel.py
@@ -11,8 +11,7 @@ from python_qt_binding import QtWidgets, QtCore
 
 class TaskPanel(Plugin):
     """
-    RQT plugin that provides simple form inputs and publishes them at 1 Hz
-    on /task_info as std_msgs/String (JSON payload).
+    RQT 插件：提供表单输入，并以 1 Hz 频率将 JSON 负载发布到 /task_info。
     """
 
     def __init__(self, context):
@@ -33,7 +32,8 @@ class TaskPanel(Plugin):
 
         # Task Label
         self.task_combo = QtWidgets.QComboBox()
-        self.task_combo.setEditable(True)
+        self.task_combo.setEditable(True
+        )
         self.task_combo.addItems(['Task 1', 'Task 2', 'Task 3'])
         self.task_combo.setCurrentIndex(-1)
         self.task_combo.lineEdit().setPlaceholderText('Select or type...')
@@ -55,10 +55,37 @@ class TaskPanel(Plugin):
         # Phantom Info
         self.phantom_combo = QtWidgets.QComboBox()
         self.phantom_combo.setEditable(True)
-        self.phantom_combo.addItems(['Abdominal Phantom', 'Lambar Phantom', 'Pork'])
+        self.phantom_combo.addItems(['Abdominal Phantom', 'Lambar Phantom', 'Silicone Phantom', 'Pork'])
         self.phantom_combo.setCurrentIndex(-1)
         self.phantom_combo.lineEdit().setPlaceholderText('Select or type...')
         form.addRow('Phantom Info:', self.phantom_combo)
+
+        # Probe Type
+        self.probe_type_combo = QtWidgets.QComboBox()
+        self.probe_type_combo.setEditable(True)
+        probe_type_options = ['Wisonic_Clover60_C5-1_convex', 'Wisonic_Clover60_L15-4_linear']
+        self.probe_type_combo.addItems(probe_type_options)
+        self.probe_type_combo.setCurrentText('Wisonic_Clover60_C5-1_convex')
+        self.probe_type_combo.lineEdit().setPlaceholderText('Select or type...')
+        form.addRow('Probe Type:', self.probe_type_combo)
+
+        # Probe Setup
+        self.probe_setup_combo = QtWidgets.QComboBox()
+        self.probe_setup_combo.setEditable(True)
+        probe_setup_options = ['Free-hand', 'Robotic']
+        self.probe_setup_combo.addItems(probe_setup_options)
+        self.probe_setup_combo.setCurrentText('Robotic')
+        self.probe_setup_combo.lineEdit().setPlaceholderText('Select or type...')
+        form.addRow('Probe Setup:', self.probe_setup_combo)
+
+        # Needle Setup
+        self.needle_setup_combo = QtWidgets.QComboBox()
+        self.needle_setup_combo.setEditable(True)
+        needle_setup_options = ['Free-hand', 'Robotic']
+        self.needle_setup_combo.addItems(needle_setup_options)
+        self.needle_setup_combo.setCurrentText('Free-hand')
+        self.needle_setup_combo.lineEdit().setPlaceholderText('Select or type...')
+        form.addRow('Needle Setup:', self.needle_setup_combo)
 
         # Comments
         self.comments_edit = QtWidgets.QTextEdit()
@@ -86,6 +113,9 @@ class TaskPanel(Plugin):
             'operator_name': self.operator_edit.text().strip(),
             'operator_skill_level': self.skill_combo.currentText().strip(),
             'phantom_info': self.phantom_combo.currentText().strip(),
+            'probe_type': self.probe_type_combo.currentText().strip(),
+            'probe_setup': self.probe_setup_combo.currentText().strip(),
+            'needle_setup': self.needle_setup_combo.currentText().strip(),
             'comments': self.comments_edit.toPlainText().strip(),
             'timestamp_iso': datetime.utcnow().isoformat() + 'Z',
         }
@@ -106,6 +136,9 @@ class TaskPanel(Plugin):
         instance_settings.set_value('operator_name', self.operator_edit.text())
         instance_settings.set_value('operator_skill_level', self.skill_combo.currentText())
         instance_settings.set_value('phantom_info', self.phantom_combo.currentText())
+        instance_settings.set_value('probe_type', self.probe_type_combo.currentText())
+        instance_settings.set_value('probe_setup', self.probe_setup_combo.currentText())
+        instance_settings.set_value('needle_setup', self.needle_setup_combo.currentText())
         instance_settings.set_value('comments', self.comments_edit.toPlainText())
 
     def restore_settings(self, plugin_settings, instance_settings):
@@ -113,5 +146,7 @@ class TaskPanel(Plugin):
         self.operator_edit.setText(instance_settings.value('operator_name', ''))
         self.skill_combo.setCurrentText(instance_settings.value('operator_skill_level', ''))
         self.phantom_combo.setCurrentText(instance_settings.value('phantom_info', ''))
+        self.probe_type_combo.setCurrentText(instance_settings.value('probe_type', 'Wisonic_Clover60_C5-1_convex'))
+        self.probe_setup_combo.setCurrentText(instance_settings.value('probe_setup', 'Robotic'))
+        self.needle_setup_combo.setCurrentText(instance_settings.value('needle_setup', 'Free-hand'))
         self.comments_edit.setPlainText(instance_settings.value('comments', ''))
-
