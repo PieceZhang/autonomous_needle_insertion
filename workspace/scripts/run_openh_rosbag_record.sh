af1c9ed161d4c9890e302dd7d4a965dc8a0bd765
Quinn Ding <qp.ding@outlook.com>
2025-12-26 21:14:39 +0800

    Add explict output directory for recording ros bags.

--- workspace/scripts/run_openh_rosbag_record.sh
+++ workspace/scripts/run_openh_rosbag_record.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+set -e
+
+OUT_DIR="/ani_ws/rosbag_recording"
+mkdir -p "$OUT_DIR"
+
+BAG_NAME="rosbag2_$(date +%Y%m%d_%H%M%S)"
 
 echo "Start recording rosbag..."
+echo "Output: $OUT_DIR/$BAG_NAME"
+echo "Output: $OUT_DIR"
 echo "Use Ctrl+C to stop recording."
 
 sleep 0.25s
 
-ros2 bag record /vega_vt/image_raw /vega_vt/camera_info \
+ros2 bag record --output "$OUT_DIR/$BAG_NAME" --topics \
+                /vega_vt/image_raw /vega_vt/camera_info \
                 /image_raw/compressed \
                 /camera/camera/color/image_raw/compressed /camera/camera/color/camera_info /camera/camera/color/metadata \
                 /camera/camera/depth/image_rect_raw/compressedDepth /camera/camera/depth/camera_info /camera/camera/depth/metadata \
