6f7484f1ae8c4f16134f77a775e38a18b802533a
ylzhang <swuzyl@gmail.com>
2025-12-28 18:40:17 +0800

    Add synchronized imaging topic to rosbag recording script

--- workspace/scripts/run_openh_rosbag_record.sh
+++ workspace/scripts/run_openh_rosbag_record.sh
@@ -26,7 +26,7 @@ ros2 bag record --output "$OUT_DIR/$BAG_NAME" --topics \
                 /decoded_coor_image/needle_tip /decoded_coor_image/needle_origin \
                 /task_info /task_info_collection_states \
                 /keyboard_listener/glyphkey_pressed /keyboard_listener/key_pressed \
-                /visualize/us_imaging/compressed
+                /visualize/us_imaging/compressed /visualize/us_imaging_sync/compressed
 #                --compression-mode file \
 #                --compression-format zstd
 
