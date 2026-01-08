b2a11ea04abcca10cba420c96744dc78aea6a61f
ylzhang <swuzyl@gmail.com>
2026-01-08 17:07:48 +0800

    feat: add termination command for rosbag recording in task1_auto.sh

--- workspace/scripts/task1_auto.sh
+++ workspace/scripts/task1_auto.sh
@@ -1,2 +1,4 @@
 source ./install/setup.bash
 ros2 launch auto_needle_insertion dataset.launch.py mode:=task1_probe_placement
+pkill -INT -f "ros2 bag record"
+echo "All ros bag recording processes have been terminated."
