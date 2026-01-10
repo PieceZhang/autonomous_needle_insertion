50a78f9bc649f5be815b08bff7e827eb9f66fca0
ylzhang <swuzyl@gmail.com>
2026-01-10 21:35:19 +0800

    feat: update rosbag command scripts to use bash env and enable strict mode

--- workspace/scripts/task4_1_auto.sh
+++ workspace/scripts/task4_1_auto.sh
@@ -1,4 +1,5 @@
 source ./install/setup.bash
 export TASK4_SUBTASK=1 && ros2 launch auto_needle_insertion dataset.launch.py mode:=find_needle_task4
 pkill -INT -f "ros2 bag record"
+bash ./scripts/send_rosbag_stop_command.sh
 echo "All ros bag recording processes have been terminated."
