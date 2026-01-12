cd7dd27d0392c29fbbcc5497e8ee3814be05b271
ylzhang <swuzyl@gmail.com>
2026-01-12 15:01:49 +0800

    fix: update docker-compose volume path for rosbag recording

--- post_processing/rosbag_decode_monitor.sh
+++ post_processing/rosbag_decode_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python  ./post_processing/rosbag_decode_monitor.py \
-      --input-dir /mnt/dataset \
+      --input-dir /mnt/dataset/rosbag_recording/ \
       --output-dir /mnt/dataset/rosbag_decoding \
       --monitoring-int 5
